b40831756b0e021a535a600c4165adb6aa2da1c1
Rodrigo Tognin <rotog@outlook.com>
2022-12-02 08:35:53 -0200

    Adicionando o arquivo de definições

---
+++ lib/definicoes.php
@@ -0,0 +1,17 @@
+<?php
+
+header('Content-Type: text/html; charset=utf-8');
+date_default_timezone_set('America/Sao_Paulo');
+
+require ('./vendor/autoload.php');
+
+define("DS", DIRECTORY_SEPARATOR);
+define("APP_VERSION", '2022.07.06-001');
+
+define ("STATUS", array(
+    '1' => 'Ativo',
+    '2' => 'Inativo'
+));
+
+define ("STATUS_ATIVO", 1);
+define ("STATUS_INATIVO", 2);
